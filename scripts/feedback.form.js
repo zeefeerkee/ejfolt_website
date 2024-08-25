8551d2de46d6492d700c82011c10a30002b4f6dc
zeefeerkee <finekii@vk.com>
2024-08-25 22:13:38 +0300

    add: TG messaging; fix: contract

--- scripts/feedback.form.js
+++ scripts/feedback.form.js
@@ -4,6 +4,8 @@ document.addEventListener("DOMContentLoaded", function () {
     const modal = new bootstrap.Modal(
         document.getElementById("staticBackdrop")
     );
+    const chatId = "768283022";
+    const botToken = "7492623821:AAFu-RKwCoZUa4oGXKcBVt7zojZC-8hOsGQ";
 
     // Функция проверки валидности формы
     function validateForm() {
@@ -21,6 +23,54 @@ document.addEventListener("DOMContentLoaded", function () {
     submitBtn.addEventListener("click", function () {
         if (form.checkValidity()) {
             // Закрываем модальное окно
+            const fieldIds = [
+                "firstName",
+                "lastName",
+                "formEmail",
+                "formTelephone",
+                "formComment",
+            ];
+            const formData = {};
+
+            fieldIds.forEach((id) => {
+                const field = form.querySelector(`#${id}`);
+                if (field) {
+                    if (field.type === "checkbox") {
+                        formData[id] = field.checked; // Для чекбоксов используем checked
+                    } else {
+                        formData[id] = field.value; // Для остальных полей используем value
+                    }
+                }
+            });
+
+            // Формируем текст сообщения
+            let messageText = "Новое сообщение!\n\n";
+            fieldIds.forEach((id) => {
+                messageText += `${formData[id]}\n`;
+            });
+            console.log(messageText);
+            fetch(`https://api.telegram.org/bot${botToken}/sendMessage`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    chat_id: chatId,
+                    text: messageText,
+                }),
+            })
+                .then((response) => response.json())
+                .then((data) => {
+                    console.log("Message sent:", data);
+                    contactFormContainer.innerHTML =
+                        "<p>Спасибо за ваше обращение!</p>";
+                    setTimeout(() => {
+                        contactFormContainer.classList.remove("visible");
+                    }, 2000);
+                })
+                .catch((error) => {
+                    console.error("Error sending message:", error);
+                });
             modal.hide();
 
             // Можно добавить код для отправки данных на сервер или другой обработки
